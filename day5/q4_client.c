878903b26a42f3dd64ad99c863fef3dfc54049ac
ashish2508 <jha250805@gmail.com>
2025-08-29 01:47:07 +0530

    work

--- day5/q4_client.c
+++ day5/q4_client.c
@@ -8,34 +8,32 @@ int main() {
   int sockfd;
   char buffer[1024];
   struct sockaddr_in servaddr;
-  socklen_t len = sizeof(servaddr);
 
   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockfd < 0) {
-    perror("socket failed");
+    perror("socket creation failed");
     exit(1);
   }
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(8080);
-  servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  servaddr.sin_addr.s_addr = INADDR_ANY; // assumes server on localhost
 
   while (1) {
-    char msg[100];
-    printf("Enter request (time/exit): ");
-    scanf("%s", msg);
+    printf("Enter message: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
 
-    sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&servaddr, len);
+    sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&servaddr,
+           sizeof(servaddr));
 
-    int n = recvfrom(sockfd, buffer, sizeof(buffer), 0,
-                     (struct sockaddr *)&servaddr, &len);
+    int n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0, NULL, NULL);
+    if (n < 0)
+      continue;
     buffer[n] = '\0';
 
-    printf("Server: %s\n", buffer);
-
-    if (strcmp(msg, "exit") == 0)
-      break;
+    printf("Server reply: %s\n", buffer);
   }
 
   close(sockfd);
--- day5/q4_server.c
+++ day5/q4_server.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
 int main() {
   int sockfd;
   char buffer[1024];
+  char reply[2048];
   struct sockaddr_in servaddr, cliaddr;
-  socklen_t len = sizeof(cliaddr);
+  socklen_t len;
 
   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockfd < 0) {
-    perror("socket failed");
+    perror("socket creation failed");
     exit(1);
   }
 
@@ -22,33 +22,30 @@ int main() {
   servaddr.sin_addr.s_addr = INADDR_ANY;
   servaddr.sin_port = htons(8080);
 
-  if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+  if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
     perror("bind failed");
     exit(1);
   }
 
-  printf("UDP Time Server running on port 8080...\n");
+  printf("UDP server running on port 8080...\n");
 
   while (1) {
-    int n = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+    len = sizeof(cliaddr);
+    int n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
                      (struct sockaddr *)&cliaddr, &len);
+    if (n < 0)
+      continue;
+
     buffer[n] = '\0';
 
-    printf("Request from client %s:%d → %s\n", inet_ntoa(cliaddr.sin_addr),
+    printf("Received from client %s:%d -> %s\n", inet_ntoa(cliaddr.sin_addr),
            ntohs(cliaddr.sin_port), buffer);
 
-    if (strcmp(buffer, "time") == 0) {
-      time_t t = time(NULL);
-      char *timestr = ctime(&t);
-      sendto(sockfd, timestr, strlen(timestr), 0, (struct sockaddr *)&cliaddr,
-             len);
-    } else if (strcmp(buffer, "exit") == 0) {
-      char msg[] = "Goodbye!";
-      sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&cliaddr, len);
-    } else {
-      char msg[] = "Invalid request!";
-      sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&cliaddr, len);
-    }
+    snprintf(reply, sizeof(reply),
+             "Hello Client (%s:%d), I got your message: %s",
+             inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port), buffer);
+
+    sendto(sockfd, reply, strlen(reply), 0, (struct sockaddr *)&cliaddr, len);
   }
 
   close(sockfd);
