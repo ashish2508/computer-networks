b6f60d3fa6ed3fb6d102134483a65e10c348d9ed
ashish2508 <jha250805@gmail.com>
2025-09-15 10:48:25 +0530

    work

---
+++ day6/server.c
@@ -0,0 +1,63 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXLINE 1024
+#define PORT 5000
+int main() {
+  struct sockaddr_in address;
+  int server_fd, client_fd;
+  int addrlen = sizeof(address);
+  char buffer[MAXLINE] = {0};
+  char *msg = "Hello from server";
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+  int opt = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    perror("setsockopt");
+    exit(EXIT_FAILURE);
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  if (listen(server_fd, 3) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+  printf("Server listening on port %d . .  .\n", PORT);
+
+  while (1) {
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&address,
+                            (socklen_t *)&addrlen)) < 0) {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+    puts("Client Connected");
+    int bytes_read = read(client_fd, buffer, MAXLINE);
+    if (bytes_read > 0) {
+      buffer[bytes_read] = '\0';
+      printf("Message from client: %s\n", buffer);
+      if (strcmp(buffer, "hi") == 0) {
+        send(client_fd, msg, strlen(msg), 0);
+        puts("Hello message sent");
+      } else {
+        char *response = "Expected 'hi'\n";
+        send(client_fd, response, strlen(response), 0);
+        puts("Error message sent");
+      }
+      close(client_fd);
+      puts("Client Disconnected");
+    }
+  }
+
+  return 0;
+}
